6bc8b88ddac3c4bfdca4a562189ede19c3a5c33d
Venkatesh <Harshith.Venkatesh@CenturyLink.com>
2020-07-21 01:58:24 +0530

    readme

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ const moment = require("moment");
 const simpleGit = require("simple-git");
 const FILE_PATH = "./data.json";
 
-const DATE = moment().subtract(1, "d").format();
-const data = {
-  date: DATE,
+const makeCommit = (x, y) => {
+  const DATE = moment()
+    .subtract(1, "y")
+    .add(1, "d")
+    .add(x, "w")
+    .add(y, "y")
+    .format();
+  const data = {
+    date: DATE,
+  };
+  jsonFile.writeFile(FILE_PATH, data, () => {
+    simpleGit().add([FILE_PATH]).commit(DATE, { "--date": DATE }).push();
+  });
 };
-jsonFile.writeFile(FILE_PATH, data, () => {
-  simpleGit().add([FILE_PATH]).commit(DATE, { "--date": DATE }).push();
-});
+makeCommit(3, 3);
