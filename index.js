be1124f90895a715d8c26dc61e147291ded28a2b
Venkatesh <Harshith.Venkatesh@CenturyLink.com>
2020-07-21 01:37:45 +0530

    Update

---
+++ index.js
@@ -0,0 +1,11 @@
+const jsonFile = require("jsonfile");
+const moment = require("moment");
+const simpleGit = require("simple-git");
+const FILE_PATH = "./data.json";
+
+const DATE = moment().format();
+const data = {
+  date: DATE,
+};
+jsonFile.writeFile(FILE_PATH, data);
+simpleGit().add([FILE_PATH]).commit(DATE, { "--date": DATE });
